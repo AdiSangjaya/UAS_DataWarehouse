13de6edc648d9e8e11ee085229f78cb34e6b672c
AdiSangjaya <adi.sangjaya@gmail.com>
2022-01-09 21:15:59 +0700

    first commit

---
+++ page/beranda.php
@@ -0,0 +1,19 @@
+<!-- judul -->
+<div class="panel">
+    <div class="panel-middle" id="judul">
+        <img src="asset/image/beranda.svg">
+        <div id="judul-text">
+            <h2 class="text-red">BERANDA</h2>
+            Halamanan Utama Administrator
+        </div>
+    </div>
+</div>
+<!-- judul -->
+<div class="panel">
+    <div class="panel-middle text-center">
+        <h1>
+            Selamat Datang, <span class="text-red">Administrator</span><br>
+        </h1>
+    </div>
+    <div class="panel-bottom"></div>
+</div>
